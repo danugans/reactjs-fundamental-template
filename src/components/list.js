0c3d902682ee497987abebd8f9ac1c7ea3cc87ba
danugans <mas.danu29@gmail.com>
2022-01-11 11:10:30 +0700

    recent

--- src/components/list.js
+++ src/components/list.js
@@ -1,1 +1,12 @@
 // code Here
+function List(props) {
+  return (
+    <div>
+      <h1>
+        {props.listData}-{props.color}
+      </h1>
+    </div>
+  );
+}
+
+export default List;
--- src/props.js
+++ src/props.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 //Import Components
+import List from "./components/list";
+
+const nameCar = "Honda";
+const colorCar = "Pink";
 
 function Props() {
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <p>On the image element using default props, namely src</p>
+      <img src="https://media.istockphoto.com/photos/generic-modern-suv-car-in-concrete-garage-picture-id1307086567?b=1&k=20&m=1307086567&s=170667a&w=0&h=NjcM6LIOkmfhyqH-zrbFU7pHCPxIABvNhWaOElm_P-E="></img>
+
+      <List listData={nameCar} color={colorCar} />
+      <List listData="BMW" />
+    </div>
   );
 }
 
